2965e3c574498c9a2ba682c201067d74c9a91957
OSCARJMG23 <oscarjulianm2112@gmail.com>
2023-12-04 21:26:58 -0500

    feat: :sparkles: Test Completed

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Solucion 1.1
+
+static int obtenerDiasNecesarios(int diaSam, int diaKelly, int differencia){
+    if (diaSam < 1 || diaKelly < 100 || differencia < 0 || differencia < 100)
+    {
+        return -1;
+    }
+    int solvedSam = differencia;
+    int solvedKelly = 0;
+    int DiasNecesarios = 0;
+    while (solvedSam >= solvedKelly)
+    {
+        solvedSam += diaSam;
+        solvedKelly += diaKelly;
+        DiasNecesarios++;
+    }
+    return DiasNecesarios;
+}
+Console.WriteLine(obtenerDiasNecesarios(4,100,1));
